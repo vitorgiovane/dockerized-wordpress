d0b22d9b467d56cad9af6eae6866690afce2bcdd
Vitor <vitorgpalves@gmail.com>
2019-02-23 18:37:00 -0300

    Displays message if file /vendor/autoload.php is not found.

--- app/wp-config.php
+++ app/wp-config.php
@@ -25,6 +25,10 @@ if(file_exists(dirname(__DIR__) . '/vendor/autoload.php')) {
 	require_once dirname(__DIR__) . '/vendor/autoload.php';
 	$dotenv = Dotenv\Dotenv::create(dirname(__DIR__));
 	$dotenv->load();
+}else{
+	echo "<h2>File <strong>/vendor/autoload.php</strong> not found!</h2>
+	<p>Run the <strong>composer install</strong> command on a terminal.</p>";
+	die;
 }
 
 define('ENVIRONMENT_DEV', 'dev');
